ee2b8465098fdb6b85f3d06cd74a6f3322a6a52c
Hoang <phuvanhoang@gmail.com>
2024-11-24 15:09:27 +0700

    a

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,25 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  console.log("first");
-  //res.send("respond with a resource");
-  next();
-});
-
-router.get("/", function (req, res, next) {
-  console.log("second");
-  res.send("respond with a resource");
+router.get("/", async function (req, res, next) {
+  const timestamp = await demo();
+  res.send("respond with a resource: " + timestamp);
 });
 
 function sleep(ms) {
-  resturn new Promise(resolve => setTimetout(resolve, ms))
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  for (let i = 0; i < 3; i++) {
+    console.log(`Starting for ${i * 1000} milisecond, i = ${i}`);
+    sleep(i * 1000).then(() => {
+      console.log(`Finished for ${i * 1000} milisecond, i = ${i}`);
+    });
+    await sleep(i * 1000);
+    console.log(`Waiting for ${i * 1000} milisecond, i = ${i}`);
+  }
+  return new Date().toLocaleString();
 }
 
 module.exports = router;
