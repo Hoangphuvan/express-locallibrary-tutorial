347b04d0edcb012dd2fb9643eb2d1696e5029281
Hoang <phuvanhoang@gmail.com>
2024-11-29 16:15:06 +0700

    update

--- models/local-library/bookinstance.js
+++ models/local-library/bookinstance.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { bookinstance_url } = require("../../constants/local-library-constant");
+const { DateTime } = require("luxon");
 
 const BookinstanceSchema = new mongoose.Schema({
   book: { type: mongoose.Schema.Types.ObjectId, ref: "Book", required: true },
@@ -17,8 +18,8 @@ BookinstanceSchema.virtual("url").get(function () {
   return `${bookinstance_url}${this._id}`;
 });
 
-BookinstanceSchema.virtual("due_back_").get(function () {
-  return this.due_back.toISOString();
+BookinstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate();
 });
 
 module.exports = mongoose.model("Bookinstance", BookinstanceSchema);
