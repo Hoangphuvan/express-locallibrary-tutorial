ec7ffbc07b3f9507a58b8bd659b24af2f8115f90
Hoang <phuvanhoang@gmail.com>
2024-12-03 11:37:01 +0700

    update

--- controllers/local-library/authorController.js
+++ controllers/local-library/authorController.js
@@ -3,6 +3,7 @@ const Book = require("../../models/local-library/book");
 const asyncHandler = require("express-async-handler");
 const { local_library_url } = require("../../constants/local-library-constant");
 const { home_url } = require("../../constants/app-constant");
+const { body, validationResult } = require("express-validator");
 
 module.exports.author_list = asyncHandler(async (req, res, next) => {
   const allAuthors = await Author.find().sort({ family_name: -1 }).exec();
@@ -36,12 +37,57 @@ module.exports.author_detail = asyncHandler(async (req, res, next) => {
 });
 
 module.exports.author_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented");
+  res.render("author-form", {
+    title: "Create Author",
+    local_library_url: local_library_url,
+    home_url: home_url,
+  });
 });
 
-module.exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented");
-});
+module.exports.author_create_post = [
+  body("first_name", "First name must be not empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("family_name", "Family name must be not empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("date_of_birth", "Date of birth is invalid.")
+    .trim()
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .escape(),
+  body("date_of_death", "Date of death is invalid.")
+    .trim()
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("author-form", {
+        title: "Create Author",
+        local_library_url: local_library_url,
+        home_url: home_url,
+        author: author,
+        errors: errors.array(),
+      });
+    } else {
+      await author.save();
+      res.redirect(author.url);
+    }
+  }),
+];
 
 module.exports.author_delete_get = asyncHandler(async (req, res, next) => {
   res.send("Not implemented");
