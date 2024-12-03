3ae73c99b480ef932c292eee73dbbfd70cfeace2
Hoang <phuvanhoang@gmail.com>
2024-12-03 21:34:48 +0700

    update

--- controllers/local-library/bookController.js
+++ controllers/local-library/bookController.js
@@ -5,6 +5,7 @@ const Author = require("../../models/local-library/author");
 const asyncHandler = require("express-async-handler");
 const { local_library_url } = require("../../constants/local-library-constant");
 const { home_url } = require("../../constants/app-constant");
+const { body, validationResult } = require("express-validator");
 
 module.exports.index = asyncHandler(async (req, res, next) => {
   const [
@@ -68,12 +69,80 @@ module.exports.book_detail = asyncHandler(async (req, res, next) => {
 });
 
 module.exports.book_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented");
+  const [allauthors, allgenres] = await Promise.all([
+    Author.find().sort({ family_name: -1 }).exec(),
+    Genre.find().sort({ name: 1 }).exec(),
+  ]);
+  res.render("book-form", {
+    title: "Create Book",
+    local_library_url: local_library_url,
+    home_url: home_url,
+    author_list: allauthors,
+    genre_list: allgenres,
+  });
 });
 
-module.exports.book_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented");
-});
+module.exports.book_create_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === undefined ? [] : [req.body.genre];
+    }
+    next();
+  },
+
+  body("title", "Title must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "Author must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("genre.*").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre,
+    });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const [allauthors, allgenres] = await Promise.all([
+        Author.find().sort({ family_name: -1 }).exec(),
+        Genre.find().sort({ name: 1 }).exec(),
+      ]);
+
+      for (const genre of allgenres) {
+        if (book.includes(genre._id)) {
+          genre.checked = true;
+        }
+      }
+
+      res.render("book-form", {
+        title: "Create Book",
+        local_library_url: local_library_url,
+        home_url: home_url,
+        book: book,
+        author_list: allauthors,
+        genre_list: allgenres,
+        errors: errors.array(),
+      });
+    } else {
+      await book.save();
+      res.redirect(book.url);
+    }
+  }),
+];
 
 module.exports.book_delete_get = asyncHandler(async (req, res, next) => {
   res.send("Not implemented");
