4f2db56323808382e93723d8ccdba89c19ac4e48
Hoang <phuvanhoang@gmail.com>
2024-12-04 11:56:00 +0700

    update

--- README.md
+++ README.md
@@ -20,3 +20,21 @@ How to run the app:
     npm run run_windows_command_prompt if run on windows os
 
 Link to generate ssl for free. expired day: Feb 24, 2025
+
+Curl test:
+Create genre:
+example curl -X POST https://sakedev.com/local-library/genre/create -d "name=Hoang"
+
+curl -X POST https://sakedev.com/local-library/author/create -d "first_name=hoang&family_name=hoàng đẹp trai&date_of_birth=2024-03-12&date_of_death=2024-03-12"
+
+Postman test:
+
+POST:
+https://sakedev.com/local-library/author/create
+body->raw ->json
+{
+"first_name":"hoang",
+"family_name": "hoàng đẹp trai",
+"date_of_birth":"2024-03-12",
+"date_of_death":"2024-03-12"
+}
--- controllers/local-library/bookinstanceController.js
+++ controllers/local-library/bookinstanceController.js
@@ -1,7 +1,9 @@
 const Bookinstance = require("../../models/local-library/bookinstance");
+const Book = require("../../models/local-library/book");
 const asyncHandler = require("express-async-handler");
 const { local_library_url } = require("../../constants/local-library-constant");
 const { home_url } = require("../../constants/app-constant");
+const { body, validationResult } = require("express-validator");
 
 module.exports.bookinstance_list = asyncHandler(async (req, res, next) => {
   const bookinstances = await Bookinstance.find().populate("book").exec();
@@ -34,22 +36,57 @@ module.exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
 
 module.exports.bookinstance_create_get = asyncHandler(
   async (req, res, next) => {
-    res.send("Not implemented");
+    const allbooks = await Book.find().sort({ title: 1 }).exec();
+    res.render("bookinstance-form", {
+      title: "Create Book Instance",
+      local_library_url: local_library_url,
+      home_url: home_url,
+      book_list: allbooks,
+    });
   }
 );
 
-module.exports.bookinstance_create_post = asyncHandler(
-  async (req, res, next) => {
-    res.send("Not implemented");
-  }
-);
+module.exports.bookinstance_create_post = [
+  body("book", "Book must be specified.").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specfied.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Due back is invalid.")
+    .optional({ values: "falsy" })
+    .trim()
+    .isISO8601(),
+  asyncHandler(async (req, res, next) => {
+    const bookinstance = new Bookinstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const allbooks = await Book.find().sort({ title: 1 }).exec();
+      res.render("bookinstance-form", {
+        title: "Create Book Instance",
+        local_library_url: local_library_url,
+        home_url: home_url,
+        book_list: allbooks,
+        bookinstance: bookinstance,
+        errors: errors.array(),
+      });
+    } else {
+      await bookinstance.save();
+      res.redirect(bookinstance.url);
+    }
+  }),
+];
 
 module.exports.bookinstance_delete_get = asyncHandler(
   async (req, res, next) => {
     res.send("Not implemented");
   }
 );
-
 module.exports.bookinstance_delete_post = asyncHandler(
   async (req, res, next) => {
     res.send("Not implemented");
